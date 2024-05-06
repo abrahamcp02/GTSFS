bd8044a91201f30758b68a60e21d33fb844538c6
Abraham Córdoba Pérez <abcp2002@gmail.com>
2024-05-06 22:21:15 +0200

    seatMap

--- frontend/src/components/SeatMap.js
+++ frontend/src/components/SeatMap.js
@@ -1,18 +1,41 @@
-import React from 'react';
-
 const SeatMap = ({ seats, onSeatSelect }) => {
+  if (!seats || !seats.length) {
+    return <p>No seats available.</p>;
+  }
+
+  // Organizando los asientos por número de fila
+  const rows = seats.reduce((acc, seat) => {
+    const { row_number } = seat;
+    acc[row_number] = acc[row_number] || [];
+    acc[row_number].push(seat);
+    return acc;
+  }, {});
+
+  const sortedRows = Object.keys(rows).sort((a, b) => parseInt(a) - parseInt(b));
+  sortedRows.forEach(row_number => {
+    rows[row_number].sort((a, b) => parseInt(a.seat_number) - parseInt(b.seat_number));
+  });
+
   return (
     <div className="seat-map">
-      {seats.map((seat) => (
-        <button
-          key={seat.id}
-          className={`seat ${seat.is_reserved ? 'true' : 'false'}`}
-          onClick={() => !seat.is_reserved && onSeatSelect(seat.id)}
-          disabled={seat.is_reserved}
-          style={{ backgroundColor: seat.is_reserved ? 'red' : 'green' }}
-        >
-          {seat.seat_number}
-        </button>
+      {sortedRows.map(row_number => (
+        <div key={row_number} className="row">
+          <div className="row-label"></div>
+          <div className="seats">
+          Fila {row_number}
+            {rows[row_number].map(seat => (
+              <button
+                key={seat.id}
+                className={`seat ${seat.is_reserved ? 'occupied' : 'available'}`}
+                onClick={() => !seat.is_reserved && onSeatSelect(seat.id)}
+                disabled={seat.is_reserved}
+                style={{ backgroundColor: seat.is_reserved ? 'red' : 'green' }}
+              >
+                {seat.seat_number}
+              </button>
+            ))}
+          </div>
+        </div>
       ))}
     </div>
   );
